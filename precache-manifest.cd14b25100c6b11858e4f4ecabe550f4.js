089dd9f598d517a6f93f64ded8a9966ac1b2de7f
Umar Bhutta <uzbhutta@uwaterloo.ca>
2020-05-22 13:52:16 -0400

    Updates

---
+++ precache-manifest.cd14b25100c6b11858e4f4ecabe550f4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ceea10f2ab9e464c5600a4cf5a60af",
+    "url": "/60s_d1/index.html"
+  },
+  {
+    "revision": "5fa584e1cb7115feda68",
+    "url": "/60s_d1/static/css/2.b230d75c.chunk.css"
+  },
+  {
+    "revision": "4b4796d22681191bbeaf",
+    "url": "/60s_d1/static/css/main.82da1a85.chunk.css"
+  },
+  {
+    "revision": "5fa584e1cb7115feda68",
+    "url": "/60s_d1/static/js/2.f6f9f000.chunk.js"
+  },
+  {
+    "revision": "d98ecab7398de6a4678f9230a85242ea",
+    "url": "/60s_d1/static/js/2.f6f9f000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b4796d22681191bbeaf",
+    "url": "/60s_d1/static/js/main.4693afd8.chunk.js"
+  },
+  {
+    "revision": "beff519a7ebc331ff00c",
+    "url": "/60s_d1/static/js/runtime-main.ade98e2d.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/60s_d1/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/60s_d1/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/60s_d1/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/60s_d1/static/media/slick.f97e3bbf.svg"
+  }
+]);
